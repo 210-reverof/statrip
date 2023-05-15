30a8fce2a5c0aa3dd3c0fcde9e8831b776dda603
이원영 <sheltonwon@gmail.com>
2023-05-15 15:40:11 +0900

    feat: 핫스팟 캐러셀 완성 #3

---
+++ frontend/src/components/home/PlanBlock.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hot-spot-block" align="center">
+  </div>
+</template>
+<script>
+
+export default {
+  name: "HotSpotBlock",
+  components: {
+  }
+};
+</script>
+
+<style>
+</style>
