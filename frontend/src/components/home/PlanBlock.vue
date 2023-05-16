360bd9d798f8056c0e064a298840f6c5ed968c8d
이원영 <sheltonwon@gmail.com>
2023-05-16 17:57:14 +0900

    feat: 경로 카드 삽입 #3

--- frontend/src/components/home/PlanBlock.vue
+++ frontend/src/components/home/PlanBlock.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="plan-block" align="center">
     <h3>New Plans</h3>
+    <div class="plan-card-container">
+      <plan-card v-for="plan in plans" :key="plan" :plan="plan" />
+    </div>
   </div>
 </template>
 <script>
+import PlanCard from './PlanCard.vue';
 
 export default {
   name: "HotSpotBlock",
   components: {
+    PlanCard
+  },
+  data() {
+    return {
+      plans: [1,2,3,4]
+    }
   }
 };
 </script>
---
+++ frontend/src/components/home/PlanCard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="plan-card">
+        <h1>카드</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.plan-card {
+    background-color: aqua;
+    width: 90%;
+    height: 40%;
+}
+</style>
