6c2579f2018c8f160ee15df99cdf4c8f5676905f
이시영 <swy05123@gmail.com>
2023-05-17 00:46:13 +0900

    tmp: 그래프 임시

--- frontend/src/components/mypage/DoughnutChart.vue
+++ frontend/src/components/mypage/DoughnutChart.vue
@@ -1,88 +1,84 @@
+<template>
+  <div class="analysis-chart-box">
+    <canvas id="chart" width="500px" height="500px"></canvas> 
+  </div>
+</template>
+
 <script>
-import { Doughnut, mixins } from 'vue-chartjs'
-const { reactiveProp } = mixins
+import {Chart, registerables} from 'chart.js';
+Chart.register(...registerables);
 
 export default {
-  extends: Doughnut,
-  mixins: [reactiveProp],
-  props: ['options'],
-  data() {
-    return {
-      value: 60
+  name: 'DoughnutChart',
+  components: { 
+  },
+  methods: {
+    fillData(chartId, label1, label2, data_true, data_false){
+      const ctx = document.getElementById(chartId).getContext('2d');
+      this.myChart = new Chart(ctx, {
+        type:'doughnut',
+        data:{
+          labels:[label1, label2],
+          datasets:[
+            {
+              backgroundColor:[
+                "#DD7445", "#DE9D11"
+              ],
+              data: [data_true, data_false]               
+            }             
+          ]
+          
+        },
+        options: { 
+          plugins: { 
+            legend: {
+              display: true, 
+              position: "bottom", 
+              labels: { 
+                boxWidth: 7, 
+                padding: 10, 
+                usePointStyle: true, 
+                pointStyle: "circle", 
+                font: { 
+                  size: 11
+                } 
+              }, 
+              fullSize: false, 
+              align: "center" 
+            }, 
+            tooltip: {
+              boxWidth: 10, 
+              bodyFont: { size: 10 } 
+            } 
+          },
+          responsive: true, 
+          maintainAspectRatio: false, 
+          layout: {
+            padding: { 
+              top: 20, 
+              bottom:20
+            } 
+          }, 
+          elements: {
+            arc: { 
+              borderWidth: 0.2
+            } 
+          }, 
+          animation: { 
+            duration: 1000
+          } 
+        } 
+      })
     }
   },
-  mounted() {
-    const drawCenterText = chart => { // 센터에 글자를 그리기 위해
-      if (chart.config.options.elements.center !== undefined) {
-        const { width, height, ctx } = chart.chart
-        ctx.restore()
-        chart.config.options.elements.center.forEach((item, index) => {
-          ctx.font = item.font
-          ctx.fillStyle = item.fillStyle
-          ctx.textBaseline = 'middle'
+  mounted() {  
+    this.fillData('chart','중립','중립 아님', 80, 20);
 
-          const textX = Math.round((width - ctx.measureText(item.text).width) / 2)
-          const textY = height / 2
-
-          ctx.fillText(item.text, textX, textY + index * 40 - 10)
-        })
-        ctx.save()
-      }
-    }
-    const setInfoForRoundedCorner = chart => {
-      if (chart.config.options.elements.arc.roundedCornersFor !== undefined) {
-        var arc = chart.getDatasetMeta(0).data[chart.config.options.elements.arc.roundedCornersFor]
-        arc.round = {
-          x: (chart.chartArea.left + chart.chartArea.right) / 2,
-          y: (chart.chartArea.top + chart.chartArea.bottom) / 2,
-          radius: (chart.outerRadius + chart.innerRadius) / 2,
-          thickness: (chart.outerRadius - chart.innerRadius) / 2 - 1,
-          backgroundColor: arc._model.backgroundColor
-        }
-      }
-    }
-    const drawRoundedCorner = chart => { // 그래프의 가장자리가 둥그스럼하게 하기 위해
-      if (chart.config.options.elements.arc.roundedCornersFor !== undefined) {
-        var ctx = chart.chart.ctx
-        var arc = chart.getDatasetMeta(0).data[chart.config.options.elements.arc.roundedCornersFor]
-        var startAngle = Math.PI / 2 - arc._view.startAngle
-        var endAngle = Math.PI / 2 - arc._view.endAngle
-
-        ctx.save()
-        ctx.translate(arc.round.x, arc.round.y)
-        console.log(arc.round.startAngle)
-        ctx.fillStyle = arc.round.backgroundColor
-        ctx.beginPath()
-        ctx.arc(
-          arc.round.radius * Math.sin(startAngle),
-          arc.round.radius * Math.cos(startAngle),
-          arc.round.thickness,
-          0,
-          2 * Math.PI
-        )
-        ctx.arc(
-          arc.round.radius * Math.sin(endAngle),
-          arc.round.radius * Math.cos(endAngle),
-          arc.round.thickness,
-          0,
-          2 * Math.PI
-        )
-        ctx.closePath()
-        ctx.fill()
-        ctx.restore()
-      }
-    }
-    this.addPlugin({
-      id: 'centerText',
-      afterDraw: drawCenterText
-    })
-    this.addPlugin({
-      id: 'roundedCornerGraph',
-      afterUpdate: setInfoForRoundedCorner,
-      afterDraw: drawRoundedCorner
-    })
-    this.renderChart(this.chartData, this.options)
-    this.htmlLegend = this.generateLegend()
+  },
+  data(){
+    return{
+      myChart: null
+    };
   }
-}
+};
 </script>
--- frontend/src/components/mypage/DoughnutChartGraph.vue
+++ frontend/src/components/mypage/DoughnutChartGraph.vue
@@ -3,12 +3,7 @@
     <div class="box">
       <div class="title">과일 선호도 조사</div>
       <div class="chart">
-        <doughnut-chart
-          ref="fruitChart"
-          :chartData="chart.data"
-          :options="chart.options"
-          id="chart"
-        ></doughnut-chart>
+        <doughnut-chart></doughnut-chart>
       </div>
     </div>
   </div>
@@ -24,47 +19,7 @@ export default {
     DoughnutChart
   },
    data() {
-    return {
-      donghnutChartOptions: {
-        legend: { display: false }, // datasets의 labels를 선언함으로 생기는 legend를 비표시로
-        cutoutPercentage: 85, // 차트의 굵기
-        elements: {
-          arc: {
-            roundedCornersFor: 0
-          },
-          center: [
-            {
-              text: '60',
-              font: 'bolder 4rem sans-serif',
-              fillStyle: '#354abd'
-            },
-            {
-              text: 'Point',
-              font: 'bold 0.8rem sans-serif',
-              fillStyle: '#222222'
-            },
-            {
-              text: '昨日比＋5',
-              font: 'bold 1rem sans-serif',
-              fillStyle: '#222222'
-            }
-          ]
-        }
-      }
-    }
-  },
-  computed: {
-    datacollection() {
-      return {
-        labels: ['score', ''],
-        datasets: [
-          {
-            data: [60, 40],
-            backgroundColor: ['#ff0058', '#ff645a']
-          }
-        ]
-      }
-    }
-  },
+    return {}
+   }
 }
 </script>
