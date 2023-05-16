38ee6db45d261ef5f06a0dc1891f1dbd92d52b09
swy0123 <swy05123@gmail.com>
2023-05-16 09:33:51 +0900

    mod: 공유게시판 1차 수정 #6

--- frontend/src/components/share/ShareList.vue
+++ frontend/src/components/share/ShareList.vue
@@ -2,18 +2,32 @@
   <b-container class="mt-3" >
     <div>
       <b-row>
-      <b-col>
-        <h2>Share</h2>
-        <h5>자신이 자랑하고싶은 여행 경로를 공유해주세요</h5>
-        <hr>
-      </b-col>
-    </b-row>
+        <b-col>
+          <h2>Share</h2>
+          <h5>자신이 자랑하고싶은 여행 경로를 공유해주세요</h5>
+          <hr>
+        </b-col>
+      </b-row>
       <b-row>
         <b-col cols="6" v-for="(article, index) in articles" :key="index">
           <div class="col-style">
-            <user-tag class="user-tag"></user-tag>
+            <b-row class="row-style">
+              <b-col cols="8">
+                <user-tag class="user-tag"></user-tag>
+              </b-col>
+              <b-col cols="4" class="like-button">
+                {{article.likes}}
+              </b-col>
+            </b-row>
             <img class="img-style"  :src=article.img alt=article.imgss><br>
-            {{article.likes}}
+            <b-row class="row-style">
+              <b-col cols="8">
+                {{article.content}}
+              </b-col>
+              <b-col cols="4">
+                {{article.regitdate}}
+              </b-col>
+            </b-row>
           </div>
         </b-col>
       </b-row>
@@ -31,16 +45,15 @@ export default {
   data: function () {
     return {
       articles: [
-          {id:1, writer:'Jessica_jj', img:"http://placehold.it/200x300?text=No-image", likes:"12"},
-          {id:1, writer:'Jessica_jj', img:"http://placehold.it/200x300?text=No-image", likes:"12"},
-          {id:1, writer:'Jessica_jj', img:"http://placehold.it/200x300?text=No-image", likes:"12"},
-          {id:1, writer:'Jessica_jj', img:"http://placehold.it/200x300?text=No-image", likes:"12"},
-          {id:1, writer:'Jessica_jj', img:"http://placehold.it/200x300?text=No-image", likes:"12"},
-          {id:1, writer:'Jessica_jj', img:"http://placehold.it/200x300?text=No-image", likes:"12"},
-          {id:1, writer:'Jessica_jj', img:"http://placehold.it/200x300?text=No-image", likes:"12"},
-          {id:1, writer:'Jessica_jj', img:"http://placehold.it/200x300?text=No-image", likes:"12"},
-          {id:1, writer:'Jessica_jj', img:"http://placehold.it/200x300?text=No-image", likes:"12"},
-          {id:1, writer:'Jessica_jj', img:"http://placehold.it/200x300?text=No-image", likes:"12"},
+          {id:1, writer:'Jessica_jj', img:"http://placehold.it/300x200?text=No-image", likes:"12", content:"이 경로 진짜 짱짱 추천입니다", regitdate:"2023.03.03"},
+          {id:1, writer:'Jessica_jj', img:"http://placehold.it/300x200?text=No-image", likes:"12", content:"이 경로 진짜 짱짱 추천입니다", regitdate:"2023.03.03"},
+          {id:1, writer:'Jessica_jj', img:"http://placehold.it/300x200?text=No-image", likes:"12", content:"이 경로 진짜 짱짱 추천입니다", regitdate:"2023.03.03"},
+          {id:1, writer:'Jessica_jj', img:"http://placehold.it/300x200?text=No-image", likes:"12", content:"이 경로 진짜 짱짱 추천입니다", regitdate:"2023.03.03"},
+          {id:1, writer:'Jessica_jj', img:"http://placehold.it/300x200?text=No-image", likes:"12", content:"이 경로 진짜 짱짱 추천입니다", regitdate:"2023.03.03"},
+          {id:1, writer:'Jessica_jj', img:"http://placehold.it/300x200?text=No-image", likes:"12", content:"이 경로 진짜 짱짱 추천입니다", regitdate:"2023.03.03"},
+          {id:1, writer:'Jessica_jj', img:"http://placehold.it/300x200?text=No-image", likes:"12", content:"이 경로 진짜 짱짱 추천입니다", regitdate:"2023.03.03"},
+          {id:1, writer:'Jessica_jj', img:"http://placehold.it/300x200?text=No-image", likes:"12", content:"이 경로 진짜 짱짱 추천입니다", regitdate:"2023.03.03"},
+          {id:1, writer:'Jessica_jj', img:"http://placehold.it/300x200?text=No-image", likes:"12", content:"이 경로 진짜 짱짱 추천입니다", regitdate:"2023.03.03"},
       ]
     }
   },
@@ -50,21 +63,19 @@ export default {
 </script>
 
 <style scoped>
-.row-style{
-  justify-content:center;
+.like-button{
+  margin: auto;
 }
 
 .col-style {
-  margin: 1%;
-  margin-bottom: 20%;
-  padding-top: 5%;
+  margin: 5%;
+  margin-bottom: 10%;
   padding-bottom: 5%;
   border-radius: 1%;
 	border-width: 1px;
-	border-color: #DDF9FF;
+	border-color: #b0b0b0;
   border-style: solid;
   box-shadow: 1px 1px 10px #b0b0b0;
-  background-color: #DDF9FF;
 }
 .img-style {
   text-align: center;
@@ -72,7 +83,6 @@ export default {
   margin-bottom:10px;
   width: 90%;
   height: auto;
-  border-radius: 3%;
   border: 1;
 }
 
