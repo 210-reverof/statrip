1bc84406acdc02a18303c2405c47838fde6d08dd
이원영 <sheltonwon@gmail.com>
2023-05-18 22:00:58 +0900

    env: add project

---
+++ backend/statrip/src/test/java/com/wonsi/statrip/StatripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonsi.statrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
