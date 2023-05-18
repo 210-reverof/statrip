87a31653592cf42e766e8e7fbca7d1d1258089f1
이원영 <sheltonwon@gmail.com>
2023-05-18 11:40:02 +0900

    feat: item 추가하기 #7

---
+++ frontend/src/components/plan/side/PlanItem.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="plan-item">
+        <img class="place-image" src="https://via.placeholder.com/300x200">
+        <div class="place-name">장소이름</div>
+        <div class="place-addr">주소</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PlanItem",
+
+}
+</script>
+
+<style scoped>
+.plan-item {
+    width: 80%;
+    height: 25%;
+    box-shadow: 0 0px 10px rgba(0, 0, 0, 0.4);
+}
+
+.place-image {
+  width: 100%;
+  height: 75%;
+  object-fit: cover;
+}
+
+.place-name {
+    font-size: 16px;
+    padding-top: 4px;
+}
+
+.place-addr{
+    font-size: 12px;
+}
+</style>
