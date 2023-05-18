3856d26db0828595a73ee2db20d57d3d2db0bb2b
이시영 <swy05123@gmail.com>
2023-05-19 00:39:22 +0900

    fix: 로그인 화면, 회원가입a 화면 수정 #10

--- frontend/src/components/auth/AuthLogin.vue
+++ frontend/src/components/auth/AuthLogin.vue
@@ -1,87 +1,120 @@
 <template>
-    <div class="auth-login">
-        <h2>Login</h2>
-        <h5>로그인</h5>
-        <hr class="separator">
-        <b-row class="row">
-            <b-col class="col-a">
-                <div class="form-group">
-                    <label for="username">Username</label>
-                    <input type="text" class="form-control" id="username" v-model="username">
-                </div>
-                <div class="form-group">
-                    <label for="password">Password</label>
-                    <input type="password" class="form-control" id="password" v-model="password">
-                </div>
-            </b-col>
-            <b-col class="col-b">
-                <div class="text-right">
-                    <button type="submit" class="btn btn-primary">Login</button>
-                </div>
-            </b-col>
+  <div class="auth-login">
+    <h2>Login</h2>
+    <h5>로그인</h5>
+    <hr />
+    <div class="form-size">
+      <b-row >
+        <b-col cols="9">
+          <div class="form-group">
+            <label for="username">ID</label>
+            <input
+              type="text"
+              class="form-control"
+              id="username"
+              v-model="username"
+            />
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input
+              type="password"
+              class="form-control"
+              id="password"
+              v-model="password"
+            />
+          </div>
+        </b-col>
+        <b-col cols="3" >
+            <div class="btn-pos">
+          <div class="login-btn" @click="submit()">Login</div>
+            </div>
+        </b-col>
+      </b-row>
+      <h6 class="join-link" @click="$router.push('/auth/joina')">
+      아직 회원이 아니신가요?
+    </h6>
+    <hr>
+    <div>
+        <b-row>
+            <b-col>간편로그인</b-col>
+            <b-col>아이콘</b-col>
+            <b-col>아이콘</b-col>
+            <b-col>아이콘</b-col>
         </b-row>
-        <div class="join-link" @click="$router.push('/auth/joina')">아직 회원이 아니신가요?</div>
     </div>
+    </div>
+
+    
+  </div>
 </template>
 
 
 <script>
 export default {
-    name: "AuthLogin",
-    data() {
-        return {
-            id: '',
-            password: ''
-        };
+  name: "AuthLogin",
+  data() {
+    return {
+      id: "",
+      password: "",
+      username: "",
+    };
+  },
+  methods: {
+    submitForm() {
+      this.id = "";
+      this.password = "";
     },
-    methods: {
-        submitForm() {
-            this.id = '';
-            this.password = '';
-        }
-    }
+    submit() {
+        console.log("llllllllll")
+    },
+  },
 };
 </script>
 
-<style>
+<style scoped>
 .auth-login {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 80%;
-    margin: 0 auto;
-    padding: 20px;
+  align-items: center;
+  width: 80%;
+  margin: 0 auto;
 }
-
-.form-control {
-    width: 100%;
-}
-
-.row {
-    width: 70%;
-}
-
-.col-a {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-}
-
-.col-b {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    justify-content: center; 
-} 
 
 .join-link {
-    text-decoration: underline;
-    cursor: pointer;
+  margin-top: 60px;
+  margin-bottom: 60px;
+  text-decoration: underline;
+  cursor: pointer;
 }
 
-.separator {
-    width: 100%;
-    margin-top: 20px;
-    margin-bottom: 20px;
+.form-size{
+  margin: 0 auto;
+    width: 30%;
+    height: 600px;
+}
+
+.form-group{
+    text-align: left;
+}
+
+.btn-pos{
+    margin-top: 28px;
+}
+
+.login-btn {
+  border: 1px solid;
+  height: 129px;
+  max-width: 150px;
+  border-color: #323F30;
+  background-color: #323F30;
+  border-radius: 5%;
+  text-align: center;
+  color: white;
+  font-size: 20px;
+  padding : 50px 0;
+}
+.login-btn:hover{
+     opacity: 1.0;
+  background-color: #375432;
+  cursor: pointer;
 }
 </style>
