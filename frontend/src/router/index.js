318e90fd2ccaecf7f9b25dece49193a6e3fe2071
이원영 <sheltonwon@gmail.com>
2023-05-19 17:24:26 +0900

    tmp: 임시 커밋

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,7 +2,29 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+import store from "@/store";
+
 Vue.use(VueRouter)
+
+const onlyAuthUser = async (to, from, next) => {
+  const checkUserInfo = store.getters["userStore/checkUserInfo"];
+  const checkToken = store.getters["userStore/checkToken"];
+  let token = sessionStorage.getItem("access-token");
+  console.log("로그인 처리 전", checkUserInfo, token);
+
+  if (checkUserInfo != null && token) {
+    console.log("토큰 유효성 체크하러 가자!!!!");
+    await store.dispatch("userStore/getUserInfo", token);
+  }
+  if (!checkToken || checkUserInfo === null) {
+    alert("로그인이 필요한 페이지입니다..");
+    // next({ name: "login" });
+    router.push({ name: "login" });
+  } else {
+    console.log("로그인 했다!!!!!!!!!!!!!.");
+    next();
+  }
+};
 
 const routes = [
   {
@@ -19,6 +41,7 @@ const routes = [
       {
         path: 'list',
         name: 'planList',
+        beforeEnter: onlyAuthUser,
         component: () => import(/* webpackChunkName: "plan" */ '@/components/plan/PlanList.vue'),
       },
       {
