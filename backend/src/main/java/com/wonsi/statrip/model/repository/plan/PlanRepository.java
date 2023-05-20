f501089af89b21e0988f7a83293c61e9d29daf59
이원영 <sheltonwon@gmail.com>
2023-05-20 22:35:53 +0900

    fix: param 오류 해결 #14

--- backend/src/main/java/com/wonsi/statrip/model/repository/plan/PlanRepository.java
+++ backend/src/main/java/com/wonsi/statrip/model/repository/plan/PlanRepository.java
@@ -5,12 +5,14 @@ import com.wonsi.statrip.model.dto.plan.PlanResDto;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 public interface PlanRepository {
     List<PlanResDto> selectMyList(String userId) throws Exception;
     int insertPlan(PlanDto planDto) throws Exception;
-    void insertPlanPresent1(Integer planId, Integer attractionId) throws Exception;
-    void insertPlanPresent2(Integer planId, Integer attractionId) throws Exception;
-    void insertPlanPresent3(Integer planId, Integer attractionId) throws Exception;
+    void insertPlanPresent1(@Param("planId") Integer planId, @Param("attractionId") Integer attractionId) throws Exception;
+    void insertPlanPresent2(@Param("planId") Integer planId, @Param("attractionId") Integer attractionId) throws Exception;
+    void insertPlanPresent3(@Param("planId") Integer planId, @Param("attractionId") Integer attractionId) throws Exception;
     void insertPlanAttrs(Integer planId, Integer attractionId, Integer ord) throws Exception;
     void updatePlan(PlanDto planDto) throws Exception;
     void deletePlan(int planId) throws Exception;
