17a699da5f991936ecb46525ee7e200211e847b1
210-reverof <sheltonwon@gmail.com>
2023-05-21 21:27:56 +0900

    feat: 오버레이 api 완성 #14

--- backend/src/main/java/com/wonsi/statrip/model/dto/plan/PlanResDto.java
+++ backend/src/main/java/com/wonsi/statrip/model/dto/plan/PlanResDto.java
@@ -28,4 +28,8 @@ public class PlanResDto {
         this.attractions = attractions;
         this.createdAt = createdAt;
     }
+
+    public PlanResDto(int planId) {
+        this.planId = planId;
+    }
 }
