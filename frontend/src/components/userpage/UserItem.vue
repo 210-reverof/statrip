78a1ea3d68c76d60521c6fbd17aa5a066d0b06c8
210-reverof <sheltonwon@gmail.com>
2023-05-24 23:27:36 +0900

    feat: 사용자 아이템 추가 #19

---
+++ frontend/src/components/userpage/UserItem.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="user-item">
+        <hr class="hr-line"/>
+        <div class="info-row">
+            <user-tag class="tag" :userId="user.userId"></user-tag>
+            <p>{{ user.name }}</p>
+            <p>{{ user.email }}</p>
+            <div v-if="doIFollow()" class="follow-btn">
+            <button>follow</button>
+            </div>
+        </div>
+        <hr class="hr-line"/>
+    </div>
+</template>
+
+<script>
+import UserTag from '@/components/common/UserTag.vue';
+import { doIFollow } from "@/api/user";
+
+export default {
+    name: "UserItem",
+    components: {
+        UserTag
+    },
+    props: {
+        user: {
+            required:true
+        }
+    }
+}
+</script>
+
+<style>
+.hr-line {
+    margin-bottom: 20px;
+    margin-top: 10px;
+}
+
+.tag {
+    margin-left: 20px;
+}
+</style>
